a8e42a73e1bc4709571f87f8d92b3c45044a37a7
Martons00 <raffaelemartone34@gmail.com>
2023-05-22 14:25:31 +0200

    Initial Commit

---
+++ InnerCompass/InnerCompassApp.swift
@@ -0,0 +1,17 @@
+//
+//  InnerCompassApp.swift
+//  InnerCompass
+//
+//  Created by Raffaele Martone on 22/05/23.
+//
+
+import SwiftUI
+
+@main
+struct InnerCompassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
